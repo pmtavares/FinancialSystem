f49e1c3ab824a7f1da6165fb7731ee93c10fc398
pmtavares <pedrotavares41@gmail.com>
2019-08-05 11:03:58 +0100

    Business Class fot Client

---
+++ FinancialSystem/Model.BUS/ClientBus.cs
@@ -0,0 +1,231 @@
+﻿using Model.Dao;
+using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.BUS
+{
+    public class ClientBus
+    {
+
+        private readonly ClientDao _clientDao;
+
+        public ClientBus(ClientDao client)
+        {
+            _clientDao = client;
+        }
+
+        public void create(Client client)
+        {
+            bool verification = true;
+
+            //Validate name
+            string name = client.name;
+            if (name == null)
+            {
+                //Message here
+                return;
+            }
+            else
+            {
+                name = client.name.Trim();
+                verification = name.Length <= 30 && name.Length > 0;
+                if (!verification)
+                {
+                    //message here
+                    return;
+                }
+
+            }
+
+            //Validate address
+            string address = client.address;
+            if (address == null)
+            {
+                //Message here
+                return;
+            }
+            else
+            {
+                address = client.address.Trim();
+                verification = address.Length <= 50 && address.Length > 0; 
+                if (!verification)
+                {
+                    //Message here
+                    return;
+                }
+
+            }
+
+            //Validate phone
+            string phone = client.phone;
+            if (phone == null)
+            {
+                //Message here
+                return;
+            }
+            else
+            {
+                phone = client.phone.Trim();
+                verification = phone.Length <= 15 && phone.Length > 6; 
+                if (!verification)
+                {
+                    //Message here
+                    return;
+                }
+
+            }
+
+            //Check for existing
+            Client objClient= new Client();
+            objClient.idClient = client.idClient;
+
+            verification = !_clientDao.find(objClient);
+
+            if (!verification)
+            {
+                //Message here
+                return;
+            }
+            //end validar duplicidade
+
+            //begin verificar duplicidade cpf retorna estado=8
+            objClient = new Client();
+            objClient.pps = client.pps;
+
+            verification = _clientDao.findClientByPps(objClient);// !objClienteDao.findClientePorcpf(objCliente1);
+            if (!verification)
+            {
+                //Message here
+                return;
+            }
+            //end validate pps 
+
+            //If there is no error
+            //Message here
+            _clientDao.createSP(objClient);
+            return;
+        }
+
+        public void update(Client client)
+        {
+            bool verification = true;
+
+            string code = client.idClient.ToString();
+            long id = 0;
+
+            if (code == null)
+            {
+                //Message here
+                return;
+            }
+            else
+            {
+                try
+                {
+                    id = Convert.ToInt64(client.idClient);
+                    verification = code.Length > 0 && code.Length < 999999; 
+
+
+                    if (!verification)
+                    {
+                        //message here
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    //message
+                    throw e;
+                }
+
+            }
+
+            string name = client.name;
+            if (name == null)
+            {
+                //Message
+                return;
+            }
+            else
+            {
+                name = client.name.Trim();
+                verification = name.Length <= 30 && name.Length > 0;
+                if (!verification)
+                {
+                    //message
+                    return;
+                }
+
+            }
+
+            string address = client.address;
+            if (address == null)
+            {
+                //Message
+                return;
+            }
+            else
+            {
+                address = client.address.Trim();
+                verification = address.Length <= 50 && address.Length > 0;
+                if (!verification)
+                {
+                    //Message
+                    return;
+                }
+
+            }
+
+            Client objClient = new Client();
+            objClient.pps = client.pps;
+
+            verification = !_clientDao.findClientByPps(objClient);
+            if (!verification)
+            {
+                //Message here
+                return;
+            }
+            //end validate pps
+
+            //If there is error
+            //Message here
+            _clientDao.update(client);
+            return;
+        }
+        public void delete(Client client)
+        {
+            bool verification = true;
+
+            //verificando se existe
+            Client objClient = new Client();
+            objClient.idClient = client.idClient;
+
+            verification = _clientDao.find(objClient);
+            if (!verification)
+            {
+                return;
+            }
+
+            _clientDao.delete(objClient);
+            return;
+        }
+
+        public bool find(Client objClient)
+        {
+            return _clientDao.find(objClient);
+        }
+
+        public List<Client> findAll()
+        {
+            return _clientDao.findAll();
+        }
+        public List<Client> findAllClients(Client objCLient)
+        {
+            return _clientDao.findAllClient(objCLient.name);
+        }
+    }
+}
--- FinancialSystem/Model.Dao/ClientDao.cs
+++ FinancialSystem/Model.Dao/ClientDao.cs
@@ -1,6 +1,7 @@
 ﻿using Model.Entity;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -25,32 +26,321 @@ namespace Model.Dao
                 "VALUES ({1}, {2}, {3}, {4})", client.name, client.address, client.phone, client.pps);
             try
             {
-
+                _command = new SqlCommand(create, _connectionDB.GetCon());
+                _connectionDB.GetCon().Open();
+                _command.ExecuteNonQuery();
             }
             catch(SqlException sql)
             {
+                
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
 
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
+            }
+        }
+
+        public void createSP(Client client)
+        {
+            //"sp_addClient{1},{2},{3},{4}", + client.idClient + "," + client.name + ", " + client.address + "," + client.pps ;
+            string create = string.Format($"sp_addClient {1},{2},{3},{4}", client.name, client.address,client.phone, client.pps);
+            try
+            {
+                _command = new SqlCommand(create, _connectionDB.GetCon());
+                _connectionDB.GetCon().Open();
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException sql)
+            {
+
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
             }
         }
 
         public void delete(Client client)
         {
-            throw new NotImplementedException();
+            string resp;
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                _command = new SqlCommand();
+
+                SqlCommand SqlCmd = new SqlCommand();
+
+                _command.Connection = _connectionDB.GetCon();
+                _command.CommandText = "sp_deleteClient";
+                _command.CommandType = CommandType.StoredProcedure;
+
+                //Initialize variables
+                SqlParameter ParId = new SqlParameter();
+                ParId.ParameterName = "@code"; //same name as in the procedure
+                ParId.SqlDbType = SqlDbType.Int;
+                ParId.Value = client.idClient;
+
+                SqlCmd.Parameters.Add(ParId);
+
+                //Execute the command
+                resp = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "Not deleted";
+
+            }
+            catch (SqlException sql)
+            {
+
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
+            }
         }
 
-        public void find(Client client)
+        public bool find(Client client)
         {
-            throw new NotImplementedException();
+            bool temRecords = false;
+            string find = string.Format($"SELECT * FROM client WHERE idClient = {1} ", client.idClient);
+            try
+            {
+                _command = new SqlCommand(find, _connectionDB.GetCon());
+                _connectionDB.GetCon().Open();
+
+                SqlDataReader reader = _command.ExecuteReader();
+                temRecords = reader.Read();
+
+                if(temRecords)
+                {
+                    client.name = reader[1].ToString();
+                    client.address = reader[2].ToString();
+                    client.phone = reader[3].ToString();
+                    client.pps = reader[4].ToString();
+                }
+
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException sql)
+            {
+
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
+            }
+            return temRecords;
         }
 
         public List<Client> findAll()
         {
-            throw new NotImplementedException();
+
+            List<Client> clientList = new List<Client>();
+            string showAll = "SELECT * FROM client ORDER BY name ASC";
+            try
+            {
+                _command = new SqlCommand(showAll, _connectionDB.GetCon());
+                _connectionDB.GetCon().Open();
+
+                SqlDataReader reader = _command.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    Client clientReceived = new Client();
+                    clientReceived.idClient = Convert.ToInt64(reader[0].ToString());
+
+                    clientReceived.name = reader[1].ToString();
+                    clientReceived.address = reader[2].ToString();
+                    clientReceived.phone = reader[3].ToString();
+                    clientReceived.pps = reader[4].ToString();
+
+                    clientList.Add(clientReceived);
+                }
+
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException sql)
+            {
+
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
+            }
+            return clientList;
+
         }
 
         public void update(Client client)
         {
-            throw new NotImplementedException();
+            string update = string.Format($"UPDATE client set name={1}, address={2}, phone={3}, pps={4}",
+                client.name, client.address, client.phone, client.pps);
+
+            try
+            {
+                _command = new SqlCommand(update, _connectionDB.GetCon());
+                _connectionDB.GetCon().Open();
+
+                SqlDataReader reader = _command.ExecuteReader();
+              
+
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException sql)
+            {
+
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
+            }
         }
+
+        public bool findClientByPps(Client client)
+        {
+            bool temRecord = false;
+            string find = string.Format($"SELECT * FROM client WHERE pps={1}", client.pps);
+
+            try
+            {
+                _command = new SqlCommand(find, _connectionDB.GetCon());
+                _connectionDB.GetCon().Open();
+
+                SqlDataReader reader = _command.ExecuteReader();
+                temRecord = reader.Read();
+                if(temRecord)
+                {
+                    client.idClient = Convert.ToInt64(reader[0].ToString());
+
+                    client.name = reader[1].ToString();
+                    client.address = reader[2].ToString();
+                    client.phone = reader[3].ToString();
+                    client.pps = reader[4].ToString();
+                }
+
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException sql)
+            {
+
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
+            }
+
+            return temRecord;
+        }
+
+        public List<Client> findAllClient(string name)
+        {
+
+            List<Client> clientList = new List<Client>();
+            string showAll = string.Format($"SELECT * FROM client WHERE name like %{1}% ORDER BY name ASC", name);
+            try
+            {
+                _command = new SqlCommand(showAll, _connectionDB.GetCon());
+                _connectionDB.GetCon().Open();
+
+                SqlDataReader reader = _command.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    Client clientReceived = new Client();
+                    clientReceived.idClient = Convert.ToInt64(reader[0].ToString());
+
+                    clientReceived.name = reader[1].ToString();
+                    clientReceived.address = reader[2].ToString();
+                    clientReceived.phone = reader[3].ToString();
+                    clientReceived.pps = reader[4].ToString();
+
+                    clientList.Add(clientReceived);
+                }
+
+                _command.ExecuteNonQuery();
+            }
+            catch (SqlException sql)
+            {
+
+                Console.WriteLine(sql);
+                throw sql;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                _connectionDB.GetCon().Close();
+                _connectionDB.CloseDB();
+            }
+            return clientList;
+
+        }
+
     }
 }
